c8c8b41f473fe89d591a5275029a57cee82a142d
UriyRatinskiy <uriy_mark@ukr.net>
2023-07-22 18:00:34 +0300

    Update contacts-api.js
    hw07

--- src/services/contacts-api.js
+++ src/services/contacts-api.js
@@ -1,18 +1,18 @@
-import axios from "axios";
+import axios from 'axios';
 
-axios.defaults.baseURL = 'https://63e93e264f3c6aa6e7ca90a8.mockapi.io';
+axios.defaults.baseURL = 'https://64b44dec0efb99d86268f3a9.mockapi.io';
 
 export async function fetchContacts() {
-    const response = await axios.get(`/contacts/contacts`);
-    return response.data;
+  const response = await axios.get(`/contacts/contacts`);
+  return response.data;
 }
 
 export async function addContact(newContact) {
-    const response = await axios.post(`/contacts/contacts`, newContact);
-    return response.data;
+  const response = await axios.post(`/contacts/contacts`, newContact);
+  return response.data;
 }
 
 export async function deleteContact(contactId) {
-    const response = await axios.delete(`/contacts/contacts/${contactId}`);
-    return response.data;
+  const response = await axios.delete(`/contacts/contacts/${contactId}`);
+  return response.data;
 }
